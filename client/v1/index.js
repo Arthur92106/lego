8c5bd772602d79573706d9c8cee013d4d12cd9ca
Arthur92106 <arthur@carreo.fr>
2026-02-03 18:08:37 +0100

    before last test

--- client/v1/index.js
+++ client/v1/index.js
@@ -2,7 +2,7 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode#invoking_strict_mode
 'use strict';
 
-console.log('🚀 This is a test for it.');
+console.log('🚀 This is the before last test.');
 
 const MY_FAVORITE_DEALERS = [
   {
